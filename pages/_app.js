fbbb9b6fdc171d260b23984ab951b1e38bdd3544
luisfe97 <luisfelipegomezr@hotmail.com>
2023-06-29 15:13:22 -0500

    cambioBootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import "@/styles/style.css";
 import { SessionProvider } from "next-auth/react";
 import "@/node_modules/bootstrap/dist/css/bootstrap.min.css";
-import Script from "next/script";
+
 import "@/node_modules/bootstrap-icons/font/bootstrap-icons.css" ;
 
 export default function App({ Component, pageProps }) {
@@ -10,21 +10,7 @@ export default function App({ Component, pageProps }) {
       <SessionProvider session={pageProps.session}>
         <Component {...pageProps} />
       </SessionProvider>
-      <Script
-        src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-        crossorigin="anonymous"
-      />
-      <Script
-        src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js"
-        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-        crossorigin="anonymous"
-      />
-      <Script
-        src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js"
-        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-        crossorigin="anonymous"
-      />
+
     </>
   );
 }
